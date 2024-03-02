4d23b9f40eb8485179bc43197e65dfb42349398d
Andres OG <andresog@192.168.1.14>
2024-03-02 09:29:43 -0600

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,302 @@
+import './App.css'
+import './normalize.css'
+import React, {useState} from 'react';
+import {datos} from './datos.jsx'
+import {recuerdos} from './datos.jsx'
+import { cartas } from './datos.jsx';
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import nutria from './img/nutria.jpg'
+import oso from './img/oso.jpg'
+import amren from './img/amren.avif'
+import libros from './img/libros.jpeg'
+
+function App() {
+  const styles = {
+    with: '100%',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  };
+
+  return (
+    <div style={styles}> 
+      <Foto />
+      <ParteDos />
+      <Parte3 bandera={true}/>
+      <Parte4 />
+    </div>
+  );
+}
+
+export default App;
+
+function Foto() {
+  const [data, setData] = useState(0);
+
+  const descripcion_corta = (texto, largo) => {
+    if (texto.length > largo) {
+      return texto.slice(0, largo) + '...';
+    } else {
+      return texto;
+    }
+  }
+
+  return (
+    <div className='fondo-foto'>
+      <div className='fondo' style={{ backgroundImage: `url(${datos[data].url})` }}></div>
+      <div className='cont'>
+        <div className='textos'>
+          <div className='con-texto'>
+            <div>
+              <h3 className='titulos-foto'>{datos[data].nombre}</h3>
+              <p className='text-foto w2'>{datos[data].descripcion}</p>
+            </div>
+            <div>
+              <button className='btn'>Ver más</button>
+            </div>
+          </div>
+          <div className='con-car'>
+          </div>
+        </div>
+        <div className='cont-foto'>
+         <HorizontalCarousel data={data} set={setData}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const HorizontalCarousel = ({data, set}) => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '80px',
+    arrows: true,
+    beforeChange: () => data >= datos.length-1 ? set(0) : set(data + 1),
+  };
+
+  return (
+    <Slider {...settings} className='carousel'>
+      {datos.map((data) => (
+        <div key={data.id} className='carousel-cont'>
+          <img className='carousel-cont-img' src={data.url} alt="" />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+function ParteDos() {
+  const [mostrarNuevoDiv, setMostrarNuevoDiv] = useState(false);
+  const [juego, setJuego] = useState(0);
+
+  const mostrarDiv = (juego) => {
+    setMostrarNuevoDiv(true);
+    setJuego(juego);
+  };
+  const ocultarDiv = () => {
+    setMostrarNuevoDiv(false);
+  }
+
+
+
+  return(
+    <div className='cont-games'>
+      <h2 className='titulos-foto'>¿Quieres jugar algo?</h2>
+      <div className='games-flex'>
+        <Games titulo="Pregunta" desc="Si contestas bien la pregunta te ganas un premio" fondo={nutria} accion={() => mostrarDiv(1)}/>
+        <Games titulo="Ordena" desc="Si ordenas bien la secuencia te ganas un premio" fondo={amren}  accion={() => mostrarDiv(2)}/>
+        <Games titulo="Pregunta" desc="Si contestas bien la pregunta te ganas un premio" fondo={libros}  accion={() => mostrarDiv(3)}/>
+      </div>
+      {mostrarNuevoDiv && juego === 1 && <Pregunta />}
+      {mostrarNuevoDiv && juego === 2 && <Ordena />}
+      {mostrarNuevoDiv && juego === 3 && <Regalo />}
+    </div>
+  );
+}
+
+function Games( {titulo, desc, fondo, accion} ) {
+  return(
+      <div className='games'>
+        <div className='filtro-games' style={{ backgroundImage: `url(${fondo})` }}></div>
+        <h3 className='quitar-margin centrar-texto'>{titulo}</h3>
+        <p className='quitar-margin centrar-texto'>{desc}</p>
+        <button className='btn btn__games' onClick={accion}>Jugar</button>
+      </div>
+  );
+}
+
+function Parte3( {bandera} ) {
+  return (
+    <div className='cont-recuerdos'>
+      <h2 className='titulos-foto'>Recuerdos</h2>
+      <div className='recuerdos'>
+      {recuerdos.map((rec) => (
+        <div key={rec.id} className=''>
+          <Recuerdos nombre={rec.nombre} img={rec.url} descripcion={rec.descripcion} />
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+function Parte4() {
+  return (
+    <div className='final'>
+      <h2 className='titulos-foto'>¡Te amo!</h2>
+      <p className='text-foto'><strong>Nota: </strong>Cada viernes habrá una actualización(:</p>
+    </div>
+  );
+}
+
+function Recuerdos({nombre, img, descripcion}) {
+  return (
+    <div className='recuerdo'>
+      <h3 className='titulos-recuerdos'>
+        {nombre}
+      </h3>
+      <Fotos img={img} />
+      <p className='texto-recuerdos'>
+        {descripcion}
+      </p>
+    </div>
+  );
+
+}
+
+function Fotos({img}){
+  return (
+    <img className='fotito' src={img} alt="Foto de mi amorcito" />
+  );
+}
+
+function Pregunta() {
+  const [respuesta, setRespuesta] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  const handleChange = (event) => {
+    setRespuesta(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (respuesta.trim() === '') {
+      setMensaje('Por favor ingresa una respuesta.');
+    } else if (respuesta.trim().toLowerCase() === 'friends') {
+      setMensaje(cartas[0]);
+    } else {
+      setMensaje('Respuesta incorrecta. Inténtalo de nuevo.');
+    }
+  };
+
+  const handleClose = () => {
+    setMensaje('');
+  };
+
+
+  return (
+    <div className='pregunta'>
+      <h2 className='titulos-foto'>Pregunta</h2>
+      <p className='text-foto'>¿Cuál es mi serie favorito?</p>
+      <input type="text" className='input' value={respuesta} onChange={handleChange} />
+      <button className='btn btn--juego' onClick={handleSubmit}>Enviar</button>
+      <VentanaFlotante mensaje={mensaje} onClose={handleClose} />
+    </div>
+  );
+}
+
+function Ordena() {
+  const [respuesta, setRespuesta] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  const handleChange = (event) => {
+    setRespuesta(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (respuesta.trim() === '') {
+      setMensaje('Por favor ingresa una respuesta.');
+    } else if (respuesta.trim().toLowerCase() === '1243') {
+      setMensaje(cartas[1]);
+    } else {
+      setMensaje('Respuesta incorrecta. Inténtalo de nuevo.');
+    }
+  };
+
+  const handleClose = () => {
+    setMensaje('');
+  };
+
+
+  return (
+    <div className='pregunta'>
+      <h2 className='titulos-foto'>Ordénalas(:</h2>
+      <div>
+        <p>
+          1. Primer beso
+        </p>
+        <p>
+          2. Primer abrazo
+        </p>
+        <p>
+          3. Primer te amo
+        </p>
+        <p>
+          4. Ser novios
+        </p>
+      </div>
+      <input type="text" className='input' value={respuesta} onChange={handleChange} placeholder='Escribe el orden sin comas ej. 2341' />
+      <button className='btn btn--juego' onClick={handleSubmit}>Enviar</button>
+      <VentanaFlotante mensaje={mensaje} onClose={handleClose} />
+    </div>
+  );
+}
+
+function Regalo() {
+  const [mensaje, setMensaje] = useState('');
+
+  const handleSubmit = () => {
+    setMensaje(cartas[2]);
+  };
+
+  const handleClose = () => {
+    setMensaje('');
+  };
+
+  return (
+    <div className='pregunta'>
+      <h2 className='titulos-foto'>Esta te la regalo por preciosa(:</h2>
+      <p className='text-foto'>Dale clic al boton antes de que me arripeinta jajaj</p>
+      <button className='btn btn--juego' onClick={handleSubmit}>Enviar</button>
+      <VentanaFlotante mensaje={mensaje} onClose={handleClose} juego={true} />
+    </div>
+  );
+}
+
+function VentanaFlotante({ mensaje, onClose, juego }) {
+  return (
+    <div className={`ventana-flotante ${mensaje ? 'visible' : ''}`}>
+      <div className="contenido">
+        <h2 style={{color:'black'}}>{mensaje.titulo}</h2>
+        { juego ? 
+          (Array.isArray(mensaje.contenido) ? 
+            mensaje.contenido.map((parrafo, index) => <p style= {{fontSize: '1.5rem'}} key={index}>{parrafo}</p>) : 
+            <p>{mensaje.contenido}</p>) : 
+          <p>{mensaje.contenido}</p>
+        }
+        <p><strong>{mensaje.team}</strong></p>
+        <p><strong>{mensaje.final}</strong></p>
+        <button className="btn btn--juego" onClick={onClose}>Cerrar</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/datos.jsx
@@ -0,0 +1,188 @@
+import img from "./img/1.jpeg";
+import img2 from "./img/2.jpeg";
+import img3 from "./img/3.jpeg";
+import img4 from "./img/4.jpeg";
+import img5 from "./img/5.jpeg";
+import img6 from "./img/6.jpeg";
+import img7 from "./img/7.jpeg";
+import img8 from "./img/8.jpeg";
+import img9 from "./img/9.jpeg";
+import img10 from "./img/10.jpeg";
+import img11 from "./img/11.jpeg";
+
+import rec1 from "./img/rec1.jpeg";
+import rec2 from "./img/rec2.jpeg";
+import rec3 from "./img/rec3.jpeg";
+import rec4 from "./img/rec4.jpeg";
+
+import rec7 from "./img/rec7.jpeg";
+import rec8 from "./img/rec8.jpeg";
+import rec9 from "./img/rec9.jpeg";
+import rec10 from "./img/rec10.jpeg";
+
+export const datos = [
+  {
+    id: 1,
+    url: img,
+    nombre: "Mi favorita",
+    descripcion:
+      "Esta es mi foto favorita para siempre. ¡Qué niña tan bonita y tierna!",
+  },
+  {
+    id: 2,
+    url: img2,
+    nombre: "Un día muy bonito",
+    descripcion:
+      "Recuerdo que ese día te veías tan bonita, que me costo trabajo no mirarte todo el tiempo.",
+  },
+  {
+    id: 3,
+    url: img3,
+    nombre: "Un día muy pintoresco",
+    descripcion:
+      "Muy bonita tu pintura al igual que tú",
+  },
+  {
+    id: 4,
+    url: img4,
+    nombre: "Me ganaste ):",
+    descripcion: "Me robaste en el turista ese día jajaja. Te amo.",
+  },
+  {
+    id: 5,
+    url: img5,
+    nombre: "Gorrito = todo más bonito",
+    descripcion: "Ponte más gorritos, te ves muy guapaaaaaa.",
+  },
+  {
+    id: 6,
+    url: img6,
+    nombre: "Pero qué bonita sonrisa",
+    descripcion: "Me encanta tu sonrisa toda hermosa.",
+  },
+  {
+    id: 7,
+    url: img7,
+    nombre: "Wooooow",
+    descripcion: "Sin palabras.",
+  },
+  {
+    id: 8,
+    url: img8,
+    nombre: "La primera foto que me mandaste",
+    descripcion: "Qué buenos recuerdos me trae esta foto",
+  },
+  {
+    id: 9,
+    url: img9,
+    nombre: "Qué bonita foto",
+    descripcion: "Me encanta esta foto, te ves muy preciosa.",
+  },
+  {
+    id: 10,
+    url: img10,
+    nombre: "Simplemente hermosa",
+    descripcion: "No hay más que decir, eres hermosa.",
+  }
+];
+
+export const recuerdos = [
+  {
+    id: 1,
+    url: rec1,
+    nombre: "La primera carta que me hiciste",
+    descripcion: "Me gustó mucho y  mira, ya casi son dos años.",
+  },
+  {
+    id: 2,
+    url: rec2,
+    nombre: "Cuando jugabamos clash",
+    descripcion: "Nunca fuiste rival, pero yo me divertía mucho.",
+  },
+  {
+    id: 3,
+    url: rec3,
+    nombre: "La primera vez que te vi",
+    descripcion: "Es una captura de cuando me explicaste donde vernos jajaja",
+  },
+  {
+    id: 4,
+    url: rec4,
+    nombre: "Tu manita",
+    descripcion:
+      "Me encanta tu manita, es tan bonita. Aún sigue viva y en perfecto estado(:",
+  },
+  {
+    id: 5,
+    url: rec10,
+    nombre: "Nuestras pulseritas",
+    descripcion: "Gracias por la explicación de qué es Bae, me encantó.",
+  },
+  {
+    id: 6,
+    url: rec7,
+    nombre: "Tu adicción jajaj",
+    descripcion: "Recuerdos muy bonitos, te superé en el parchis jsjs",
+  },
+  {
+    id: 7,
+    url: rec8,
+    nombre: "Nustra primera mojada jaja",
+    descripcion:
+      "No fue la peor, de esa no hay foto jajaja, pero sí fue la primera.",
+  },
+  {
+    id: 8,
+    url: rec9,
+    nombre: "Tu dibujo",
+    descripcion: "¿Sigue siendo así?",
+  },
+];
+
+export const cartas = [
+  {
+    id: 1,
+    titulo: "Holi",
+    contenido:
+      "Quiero comenzar diciendo que es el primer San Valentín al que realmente le doy importancia jajaja, normalmente estaba solo o con Joel en el salón, desesperados por ya irnos, aburridos o sentados en las llantas viendo a los demás ser felices o falsamente felices jajaja. En fin, siempre supe que solo disfrutaría un San Valentín cuando mis amigos no tuvieran novia o fueran igual de asociales que yo, pero realmente nunca me imaginé que iba a ser porque me iba a enamorar perdidamente de alguien. \nAhora tengo con quien compartir el día jajaja. Y no solo San Valentín, sino todos mis días, tengo a alguien que me hace muy feliz y a quien quiero hacer muy feliz, tengo a alguien con quien disfruto pasar mi tiempo, con quien disfruto hablar de cualquier cosa, a quien abrazar, en quien apoyarme y apoyar, tengo a una maravillosa mujer como novia y tengo la fortuna de que seas tú.",
+    team: '\nFeliz San Valentín, Preciosa. Te amo',
+    final: 'Andres.'
+  },
+  {
+    id: 2,
+    titulo: "Gracias",
+    contenido: "Aquí va la lista de las cosas por las que estoy agradecido contigo. Gracias por siempre apoyarme, gracias por hacerme tan feliz, gracias por siempre estar, tanto en momentos buenos como malos, gracias por ser tan buena novia como eres, gracias por ser tan maravillosa, gracias por creer en mí, gracias por cada abrazo que me has dado, gracias por cada momento compartido, gracias por cada sonrisa, gracias por cada día, gracias por cada mensaje… gracias por existir, gracias porque por con cada cosa que he mencionado, has contribuido a que sea mejor persona, a querer esforzarme por lograr mis metas, porque sin esa energía que me das, todo sería más difícil.",
+    team: '\nTe amo mucho.',
+    final: 'Andres.'
+  },
+  {
+    id: 3,
+    titulo: "Lista de cosas que amo de ti",
+    contenido: [
+        'Tu sonrisa',
+        'Tu determinación',
+        'Tus ojitos',
+        'Tus labios',
+        'Tus abrazos',
+        'Tus besos',
+        'Tus detalles',
+        'Tus mensajes',
+        'Verte mientras hablas',
+        'Tus hoyuelitos',
+        'Tu alegría',
+        'Tu amabilidad',
+        'Tu fortaleza',
+        'Tus historias',
+        'La forma en la que platicas tus historias (Podría pasar todo el día oyéndote)',
+        'La forma en la que me tratas',
+        '...',
+        'Tu dedicación',
+        'Tus manitas',
+        'Tomarte de la cintura',
+        'Cuando sales de tu casa y escucharte decir “Holi, guapo” o “Holi, mi amor”',
+        'Casi todo',
+    ],
+    team: "Te amo",
+    final: "Andres."
+  },
+];
